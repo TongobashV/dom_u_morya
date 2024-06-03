2d1576fe349a5f217692d7b37b48d9550b3399ad
Vasiliy Tongobash <v.tonhobash@gmail.com>
2024-06-03 09:17:18 +0300

    подключил библиотеку sorl-thumbnail (https://sorl-thumbnail.readthedocs.io/en/latest/)

--- dom_u_morya/settings.py
+++ dom_u_morya/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "houses.apps.HousesConfig"
+    "houses.apps.HousesConfig",
+    "sorl.thumbnail"
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = "/media/"
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+        'LOCATION': BASE_DIR / 'cache',
+    }
+}
