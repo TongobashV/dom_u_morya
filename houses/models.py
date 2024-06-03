aeff480c869b53da1bb21b551eaa371e92953ab9
Vasiliy Tongobash <v.tonhobash@gmail.com>
2024-06-03 08:49:35 +0300

    добавил поле для хранения фото

--- houses/models.py
+++ houses/models.py
@@ -6,7 +6,7 @@ class House(models.Model):
     name = models.CharField("название", max_length=50)
     price = models.IntegerField("цена")
     description = models.TextField("описание")
-
+    photo = models.ImageField("фотография", upload_to="houses/photo", default="", blank=True)
     class Meta:
         verbose_name = "дом"
         verbose_name_plural = "дома"
