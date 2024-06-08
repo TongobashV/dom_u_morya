51c06abf38526472bbf5cc2eb087fc688a54230c
Vasiliy Tongobash <v.tonhobash@gmail.com>
2024-06-08 09:41:10 +0300

    добавил вывод формы

--- houses/views.py
+++ houses/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from houses.models import House
+from orders.forms import OrderForm
 
-# Create your views here.
+
 def houses_list(request):
     houses = House.objects.all()
     for house in houses:
@@ -9,5 +10,6 @@ def houses_list(request):
     return render(request, "houses/houses_list.html", {"houses": houses})
 
 def house_detail(request, house_id):
+    form = OrderForm()
     house = get_object_or_404(House, id=house_id)
-    return render(request, "houses/houses_detail.html", {"house": house})
+    return render(request, "houses/houses_detail.html", {"house": house, "form": form})
