a7ba9e4d26f36fc95880fd3c83d27477e1a29175
Vasiliy Tongobash <v.tonhobash@gmail.com>
2024-06-08 09:29:55 +0300

    добавил пути для ссылок

--- dom_u_morya/urls.py
+++ dom_u_morya/urls.py
@@ -16,14 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from houses.views import houses_list
 from django.conf.urls.static import static
 from django.conf import settings
 from houses.views import houses_list, house_detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', houses_list),
-    path("<int:house_id>", house_detail)
+    path('', houses_list, name="home"),
+    path("<int:house_id>", house_detail, name="house")
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
