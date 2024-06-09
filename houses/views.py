55d5bbbc22701df2d1daf97c07e5803527976777
Vasiliy Tongobash <v.tonhobash@gmail.com>
2024-06-09 16:10:36 +0300

    Перенес получение дома, до создания формы

--- houses/views.py
+++ houses/views.py
@@ -10,9 +10,10 @@ def houses_list(request):
     return render(request, "houses/houses_list.html", {"houses": houses})
 
 def house_detail(request, house_id):
-    form = OrderForm(request.POST or None)
+    house = get_object_or_404(House, id=house_id)
+    form = OrderForm(request.POST or None, initial={"house":house})
     if request.method == "POST":
         if form.is_valid():
             form.save()
-    house = get_object_or_404(House, id=house_id)
+
     return render(request, "houses/houses_detail.html", {"house": house, "form": form})
