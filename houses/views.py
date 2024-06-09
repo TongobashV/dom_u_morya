746b10879107460cf8e2edf9323eae579147f1b8
Vasiliy Tongobash <v.tonhobash@gmail.com>
2024-06-09 14:51:44 +0300

    Внес изменения чтобы данные регистрации пользователя сохранялись в БД

--- houses/views.py
+++ houses/views.py
@@ -10,6 +10,9 @@ def houses_list(request):
     return render(request, "houses/houses_list.html", {"houses": houses})
 
 def house_detail(request, house_id):
-    form = OrderForm()
+    form = OrderForm(request.POST or None)
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
     house = get_object_or_404(House, id=house_id)
     return render(request, "houses/houses_detail.html", {"house": house, "form": form})
