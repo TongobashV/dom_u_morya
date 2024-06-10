6c37eb146f54d7ea6725a68458374ccbc56a9bae
Vasiliy Tongobash <v.tonhobash@gmail.com>
2024-06-10 08:37:29 +0300

    Передал форму в шаблон Прописал сохранение значений при выборе а также ограничения в фильтре минимум и максимум

--- houses/views.py
+++ houses/views.py
@@ -3,13 +3,19 @@ from houses.models import House
 from orders.forms import OrderForm
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from houses.forms import HousesFilterForms
+
 
 
 def houses_list(request):
     houses = House.objects.all()
-    for house in houses:
-        print(house.name, house.price)
-    return render(request, "houses/houses_list.html", {"houses": houses})
+    form = HousesFilterForms(request.GET)
+    if form.is_valid():
+        if form.cleaned_data["min_price"]:
+            houses = houses.filter(price__gte=form.cleaned_data["min_price"])
+        if form.cleaned_data["max_price"]:
+            houses = houses.filter(price__lte=form.cleaned_data["max_price"])
+    return render(request, "houses/houses_list.html", {"houses": houses, "form": form})
 
 def house_detail(request, house_id):
     house = get_object_or_404(House, id=house_id)
