aa839077bc52e5effe726d2ed6730d6c7922c02c
Vasiliy Tongobash <v.tonhobash@gmail.com>
2024-06-11 17:41:02 +0300

    Вывел поле active в админке

--- houses/admin.py
+++ houses/admin.py
@@ -5,4 +5,5 @@ from houses.models import House
 # Register your models here.
 @admin.register(House)
 class HouseAdmin(admin.ModelAdmin):
-    list_display = ["name", "price"]
+    list_display = ["name", "price", "active"]
+    list_filter = ["active"]
