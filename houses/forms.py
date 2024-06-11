13ca3392d85ab59aa7acf6c144861ade4b87073a
Vasiliy Tongobash <v.tonhobash@gmail.com>
2024-06-11 18:28:43 +0300

    Добавил возможность поиска непосредственно по названию, описанию

--- houses/forms.py
+++ houses/forms.py
@@ -3,3 +3,4 @@ from django import forms
 class HousesFilterForms(forms.Form):
     min_price = forms.IntegerField(label="от", required=False)
     max_price = forms.IntegerField(label="до", required=False)
+    query = forms.CharField(label="описание", required=False)
