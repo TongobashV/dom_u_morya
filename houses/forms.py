aec7dd7063a53bb355931c9f071062523f8f8d6a
Vasiliy Tongobash <v.tonhobash@gmail.com>
2024-06-12 23:54:23 +0300

    Добавил возможность менять порядок сортировки

--- houses/forms.py
+++ houses/forms.py
@@ -4,3 +4,8 @@ class HousesFilterForms(forms.Form):
     min_price = forms.IntegerField(label="от", required=False)
     max_price = forms.IntegerField(label="до", required=False)
     query = forms.CharField(label="описание", required=False)
+    ordering = forms.ChoiceField(label="сортировка", required=False, choices=[
+        ("name", "по алфавиту"),
+        ("price", "дешевые сверху"),
+        ("-price", "дорогие сверху")
+    ])
